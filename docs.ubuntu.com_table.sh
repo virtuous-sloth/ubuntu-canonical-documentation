e480784c0e40fc92018ae85cbae7d1e3bc00cb4b
Bruce Elrick <bruce@elrick.ca>
2023-06-12 15:26:05 -0600

    Add url variable to make more generic

--- docs.ubuntu.com_table.sh
+++ docs.ubuntu.com_table.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
 
 name=$(basename "$0" | sed -e 's/\.sh$//')
+url=$(echo $name | sed -e 's/_.*//' | tr - /)
 
-curl --silent https://docs.ubuntu.com/ \
+curl --silent "https://$url" \
 	| htmlparser 'a[class="p-link"] json{}' \
 	| jq -r '["Doc Site", "URL"], (.[] | [.text, .href]) | @csv' \
 	| tr '"' "\`" \
